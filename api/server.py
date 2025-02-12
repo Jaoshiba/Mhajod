4b4b6a450d0b6abf62fb46e272e42d5cda6e7da9
Jaoshiba <blackspell909@gmail.com>
2025-02-12 09:43:37 +0700

    Update server.py

--- api/server.py
+++ api/server.py
@@ -1,8 +1,11 @@
 from flask import Flask,jsonify,blueprints
 from flask_cors import CORS
+from ocr_api_on import ocr
 
 app = Flask(__name__)
 CORS(app)
+
+app.register_blueprint(ocr)
 
 if __name__ == '__main__':
     app.run(debug=True)
